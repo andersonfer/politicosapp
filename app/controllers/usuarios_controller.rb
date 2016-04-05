535d122f971922f94b3eaac212785e1e94f6186e
Bruno Rolim <brolim@gmail.com>
2016-04-05 16:55:46 -0300

    menu interno mais inteligente e rotas para new e index de usuário

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -4,4 +4,7 @@ class UsuariosController < ApplicationController
     @usuarios = Usuario.all
   end
 
+  def new
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+	def menu_interno
+		if request.path.gsub(/\?.*/, '').include?(usuarios_path)
+			menu_usuarios
+		end
+	end
+
+	def menu_usuarios
+		links = {}
+		links['lista'] = {'path'=>usuarios_path}
+		links['novo'] = {'path'=>new_usuario_path}
+
+		links.each do |nome, dados|
+			if dados['path']==request.path.gsub(/\?.*/, '')
+				links[nome]['ativo'] = true
+			else
+				links[nome]['ativo'] = false
+			end 
+		end
+		links
+	end
+
 end
